2ecc18d510b9a49a71664673a3d943dd228b172d
Dongkyu Seo <sdq9039@naver.com>
2022-05-09 17:28:26 +0900

    22.05.09_java

---
+++ JAVA/SelfStudyJava/chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+/*
+ 작성일: 2022.05.09
+ */
+import javax.swing.*;
+
+public class Hello {
+	// 프로그램 실행 진입점 - main
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문 println
+		System.out.println("Hello, Java");
+
+	}// end of main
+
+}// end of class
